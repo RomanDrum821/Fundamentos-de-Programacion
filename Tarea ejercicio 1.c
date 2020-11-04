e37eef9cc292c021d7e5e5863ecdc9056648c6ab
Hector Isaac Roman Vazquez <72269944+RomanDrum821@users.noreply.github.com>
2020-11-04 10:09:27 -0600

    Create Tarea ejercicio 1.c

---
+++ Tarea ejercicio 1.c
@@ -0,0 +1,27 @@
+//Tarea 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    
+    
+	float x;
+	float y;
+	float z;
+	
+	
+	x = ((6.18)/(5)) * ((((14.1)-(4))/2)*(((14.1)-(4))/2));
+	y = (((8.9) * (34))/(17));
+	z = (8-7);
+	
+    
+    printf("\n%f",x);
+  	printf("\n%f",y);
+	printf("\n%f",z);
+	
+	
+	
+	    
+    return (EXIT_SUCCESS);
+
+}
