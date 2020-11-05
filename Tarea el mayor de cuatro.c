66aa8ec1717c836e0ae1f69a6e0374983dc9ecf1
Hector Isaac Roman Vazquez <72269944+RomanDrum821@users.noreply.github.com>
2020-11-05 16:21:37 -0600

    Create Tarea el mayor de cuatro.c
    Tarea el mayor de cuatro

---
+++ Tarea el mayor de cuatro.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv)
+{
+	
+	float w = 16;
+	float x = 10;
+	float y = 15;
+	float z = 13;
+
+	//bloque para el veradero o falso
+	
+	printf 	("\n w: %f", w); 
+	printf 	("\n x: %f", x); 
+	printf 	("\n y: %f", y);
+	printf 	("\n z: %f", z); 
+		
+	if ( w > x && w > y && w > z)
+	{
+		printf 	("\n El mayor es w");
+	}
+		else
+		{
+			if ( x > w && x > y && x > z)
+			{
+			printf 	("\n El mayor es x");	
+			}
+				if ( y > w && y > x && y > z)
+				{
+				printf 	("\n El mayor es y");	
+				}
+					else
+					{
+					printf ("\n El mayor es z");
+					}
+		}
+	
+
+	return 0;	
+	
+}
