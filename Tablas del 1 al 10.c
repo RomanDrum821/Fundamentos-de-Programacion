b75b2eef7f8b0c1712f9f0e0a04f90a117c715c9
Hector Isaac Roman Vazquez <72269944+RomanDrum821@users.noreply.github.com>
2020-11-10 16:00:43 -0600

    Tablas con while anidados

---
+++ Tablas del 1 al 10.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+//comienza el programa
+
+int main(int argc, char** argv) 
+{
+	int x=1;
+	int y=1;
+	int r;
+
+	while (x<=10)
+
+	{	
+	printf("\n \n Tabla del %d \n",x);
+		while (y<=10)
+
+		{
+		
+			r = x * y;
+			printf("\n %d * %d = %d",x,y,r);
+			y = y + 1 ;
+			}
+	y = 1;
+	x = x + 1 ;
+	}
+
+return 0;
+
+}
