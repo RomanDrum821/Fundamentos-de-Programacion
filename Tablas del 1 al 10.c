83827f4400ede1643cc774af51b0a17b52e2c20d
Hector Isaac Roman Vazquez <72269944+RomanDrum821@users.noreply.github.com>
2020-11-17 15:07:45 -0600

    Update Tablas del 1 al 10.c

--- Tablas del 1 al 10.c
+++ Tablas del 1 al 10.c
@@ -8,22 +8,25 @@ int main(int argc, char** argv)
 	int x=1;
 	int y=1;
 	int r;
-
+	
 	while (x<=10)
 
 	{	
-	printf("\n \n Tabla del %d \n",x);
+	printf("\n \n Tabla del  %d \n",x);
+	
 		while (y<=10)
 
 		{
 		
 			r = x * y;
-			printf("\n %d * %d = %d",x,y,r);
+			printf("\n %d * %d = %d\n",y,x,r);			
 			y = y + 1 ;
 			}
+
 	y = 1;
 	x = x + 1 ;
 	}
+	
 
 return 0;
 
