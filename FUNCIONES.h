34cf7494f41886bae776832ea3602217e6cbcaff
Hector Isaac Roman Vazquez <72269944+RomanDrum821@users.noreply.github.com>
2021-01-18 15:07:06 -0600

    Archivos de cabecera

--- FUNCIONES.h
+++ FUNCIONES.h
@@ -1,15 +1,19 @@
-/*contiene declaraciones de variables y funciones*/
-#ifndef NEWFILE_H
-#define NEWFILE_H
+#ifndef FUNCIONES_H
+#define	FUNCIONES_H
 
-// Declarar funciones
+float pi= 3.1452;
 
-int suma(int a, int b);
-int resta(int a, int b);
-char primer_caracter(char* cadena);
-double area_circulo(int radio);
-
-// Declarar variables
-
+int suma (int a, int b){
+    return a+b;
+}
+int resta (int a, int b){
+    return a-b;
+}
+char primer_caracter (char* cadena){
+    return cadena [0];
+}
+double area_circulo(int radio){
+   return pi*radio*radio;
+}
 
 #endif
